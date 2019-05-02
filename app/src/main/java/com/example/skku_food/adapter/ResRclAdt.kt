0aa97f68b78d08beca66dccd5509d51c1c992bc4
BearHunter49 <38463645+BearHunter49@users.noreply.github.com>
2019-05-02 15:07:28 +0900

    Tournament 사진 Resource에 넣음 (남은 사항: Back키 처리)

---
+++ app/src/main/java/com/example/skku_food/adapter/ResRclAdt.kt
@@ -0,0 +1,46 @@
+package com.example.skku_food.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.skku_food.R
+import com.example.skku_food.data.ResSimpleData
+import com.example.skku_food.menuActivity.ResMenuActivity
+import kotlinx.android.synthetic.main.res_item.view.*
+
+class ResRclAdt(private val items:List<ResSimpleData>, private val menu: String):RecyclerView.Adapter<ResRclAdt.ResViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ResViewHolder(parent)
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ResViewHolder, position: Int) {
+
+        items[position].let { item ->
+            with(holder){
+                nm.text = item.name
+                ph.text = item.phone
+                itemView.btn_res.setOnClickListener{
+                    with(itemView.context) {
+                        val intent = Intent(this, ResMenuActivity::class.java)
+                        intent.putExtra("name", item.name)
+                        intent.putExtra("menu", menu)
+                        this.startActivity(intent)
+                    }
+                }
+            }
+        }
+    }
+
+
+    inner class ResViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
+        LayoutInflater
+        .from(parent.context)
+        .inflate(R.layout.res_item, parent, false)){
+
+        val nm = itemView.text_resNM
+        val ph = itemView.text_resPH
+    }
+}
